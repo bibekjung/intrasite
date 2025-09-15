e479097c7b23fec241d43aeba222bfa880287f52
Bibek Thapa <bibek.thapa@cibnepal.org.np>
2025-09-15 15:14:54 +0545

    t

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,6 @@
-
 export default function Login() {
-console.log("ddd")  
-return (
+  return (
     <div className="min-h-screen bg-gray-100">
-
       <div className="flex h-screen items-center justify-center">
         <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
           <h2 className="mb-6 text-center text-2xl font-bold text-gray-800">
