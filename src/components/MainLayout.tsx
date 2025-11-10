eda9390fa8bff09e0e4e4e855aab622468f13504
lamshalp <prawal.lamshal@cibnepal.org.np>
2025-11-10 11:28:07 +0545

    Update components and add new features

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -84,7 +84,7 @@ export default function MainLayout() {
           </div>
         </header>
 
-        <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
+        <main className="flex-1 p-6 bg-white overflow-y-auto">
           <Outlet />
         </main>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import { ToasterProvider } from './components/ui/toaster';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <ToasterProvider>
+      <App />
+    </ToasterProvider>
   </Provider>,
 );
